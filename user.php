9e0cdf382d4f02bfcd36775539cbcab6ff7aab69
Nattapong Pailom <kosutk0807853136@gmail.com>
2023-09-19 16:57:03 +0700

    server php PDO

---
+++ user.php
@@ -0,0 +1,77 @@
+<?php 
+
+
+    session_start();
+    require_once 'config/db.php';
+    if (!isset($_SESSION['user_login'])) {
+        $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ';
+        header("location: signin.php");
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+    
+   
+    <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Book</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link btn btn-danger" href="logout.php">Logout</a>
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <h1> <?php 
+        if (isset($_SESSION['user_login'])) {
+            $user_id = $_SESSION['user_login'];
+            $stmt = $conn->query("SELECT * FROM users WHERE id = $user_id");
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+          }
+        ?>
+        <h3 class="mt-4">Welcome, <?php echo $row['firstname'] . ' ' . $row['lastname'];?></h3></h1>
+    <p class="lead">This is website for book loan system</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-black">กู้ยืมหนังสือ</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  </footer>
+</div>
+
+<!-- <div class="container">
+        <?php 
+        if (isset($_SESSION['user_login'])) {
+            $user_id = $_SESSION['user_login'];
+            $stmt = $conn->query("SELECT * FROM users WHERE id = $user_id");
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+          }
+        ?>
+        <h3 class="mt-4">Welcome, <?php echo $row['firstname'] . ' ' . $row['lastname'];?></h3>
+    </div> -->
+
+  
+
+</body>
+</body>
+</html>
